d478e45b0fb830a4d239b5022c5504da5a252056
Adrian <adrian@arch.sunplaza.net>
2015-06-12 23:22:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android/silab/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android.silab;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO populate spinner
+        Spinner spinner = (Spinner) findViewById(R.id.emoji_selector);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.emoji_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        //TODO setup character hashtable
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void render(){
+        /* The engine of this app. Takes user string and selected emoji
+         * and renders the character using the emoji.
+         */
+
+
+    }
+}

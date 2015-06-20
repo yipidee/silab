76491c48acb014706475b26d600ab6cb6006419a
Adrian Connolly <amjconnolly@gmail.com>
2015-06-20 16:53:24 +0900

    image type, not working

--- app/src/main/java/com/example/android/silab/MainActivity.java
+++ app/src/main/java/com/example/android/silab/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.android.silab;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +14,10 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Hashtable;
 import java.util.List;
 
@@ -51,7 +56,6 @@ public class MainActivity extends Activity {
 
         // character map hashtable from helper
         charMap = sh.getCharMap();
-
     }
 
     // this is automatically created, did touch this function
@@ -84,15 +88,10 @@ public class MainActivity extends Activity {
          * and renders the character using the emoji.
          */
 
-        /*//For test Purposes make user input currently available characters
-        //TODO remove this test data
-        userInput.setText("b a cf");
-        */
-
         //Variables to contain user input and output
         String userString = "";
         String emojified = "";
-        
+
         //handle to the user input
         EditText userInput = (EditText)findViewById(R.id.user_text);
         userString = userInput.getText().toString();
@@ -111,21 +110,53 @@ public class MainActivity extends Activity {
     }
 
     public void share(View v){
-        /* Bare bones intent to send rendered string.
+        /* Bare bones intent to send renderView as image.
          * Tested with:
-         *   Hangouts:   NG
-         *   gmail:      OK
-         *   Facebook:   NG
-         *   FB mess:    NG
+         *   Hangouts:
+         *   gmail:
+         *   Facebook:
+         *   FB mess:
          */
 
         if(isRendered){
-            Intent sendIntent = new Intent();
-            sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, renderView.getText());
-            sendIntent.setType("text/plain");
-            startActivity(sendIntent);
-        }
+
+            // Create bitmap
+            Bitmap bm = Bitmap.createBitmap(renderView.getDrawingCache());
+            //Bitmap bm= Bitmap.createBitmap(view_bm,0,0,renderView.getWidth(),(int)(renderView.getLineCount()*renderView.getTextSize()));
+            //Canvas c = new Canvas(bm); // create a canvas from the bitmap
+
+            // file to store PNG image of renderView content
+            // this is only a file object, it doesn't create a file on disc
+            File f = new File(Environment.getExternalStorageDirectory()+File.separator, "tmp_emojified.png");
+
+            try {
+                f.createNewFile(); // create the file
+            }catch(IOException e){
+                // file creation fails for whatever reason
+                displayDialog(R.string.error_file_open);
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.PNG, 0, bos);
+            byte[] bitmapdata = bos.toByteArray();
+
+            try{
+                FileOutputStream fos = new FileOutputStream(f);
+                fos.write(bitmapdata);
+                fos.flush();
+                fos.close();
+            }catch(IOException e){
+                // catchs error thrown when problem with file IO
+                e.printStackTrace();
+            }
+/*
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(f));
+            shareIntent.setType("image/png");
+            startActivity(Intent.createChooser(shareIntent, "Choose your share app!"));
+*/        }
     }
 
     protected void displayDialog(int errorMessage){
@@ -155,6 +186,7 @@ public class MainActivity extends Activity {
 
         //handle to the render view
         renderView = (TextView)findViewById(R.id.render_textview);
+        renderView.setDrawingCacheEnabled(true);
 
         for (int i=0; i<input.length; i++) {
 
